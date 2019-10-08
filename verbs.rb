befb8c06ca3b081731aea1cd503baee482ef7d7a
Feña Agar <fernando.agar@solarisbank.de>
2019-10-08 20:27:51 +0200

    HTTP Verbs

---
+++ verbs.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+# CRUD
+# root route
+get '/' do
+  puts "I'm visible in the terminal"
+  "I'm visible in the browser"
+end
+
+# Create
+post '/post_route' do
+  "I posted it"
+end
+
+# Read
+get '/get_route' do
+  "I got it"
+end
+
+# Update
+put '/put_route' do
+  "I put it"
+end
+
+# Delete
+delete '/delete_route' do
+  "I deleted it"
+end
